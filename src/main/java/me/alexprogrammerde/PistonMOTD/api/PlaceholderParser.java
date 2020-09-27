e95fe21da111ebd074a4ea755aff712563c8bdbb
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2020-09-27 22:32:50 +0200

    Add some documentation

--- src/main/java/me/alexprogrammerde/PistonMOTD/api/PlaceholderParser.java
+++ src/main/java/me/alexprogrammerde/PistonMOTD/api/PlaceholderParser.java
@@ -1,5 +1,10 @@
 package me.alexprogrammerde.PistonMOTD.api;
 
 public interface PlaceholderParser {
+    /**
+     * Parse the placeholders of
+     * @param text Current string
+     * @return Parsed string
+     */
     String parseString(String text);
 }
--- src/main/java/me/alexprogrammerde/PistonMOTD/api/PlaceholderUtil.java
+++ src/main/java/me/alexprogrammerde/PistonMOTD/api/PlaceholderUtil.java
@@ -7,6 +7,11 @@ import java.util.List;
 public class PlaceholderUtil {
     private static final List<PlaceholderParser> placeholders = new ArrayList<>();
 
+    /**
+     * Parse a string and run all parsers on him
+     * @param text A string with placeholders to get parsed
+     * @return A completely parsed string
+     */
     public static String parseText(String text) {
         String returnedstring = text;
 
@@ -17,10 +22,18 @@ public class PlaceholderUtil {
         return returnedstring;
     }
 
+    /**
+     * Register a parser to make him parse some placeholders
+     * @param parser A parser to register
+     */
     public static void registerParser(@Nonnull PlaceholderParser parser) {
         placeholders.add(parser);
     }
 
+    /**
+     * Unregister a parser to stop him from parsing
+     * @param parser The parser to unregister
+     */
     public static void unregisterParser(@Nonnull PlaceholderParser parser) {
         placeholders.remove(parser);
     }
