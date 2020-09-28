a7d500ddcbabe2bf6dfccc15f8b7cfedef59fda7
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2020-09-28 09:37:16 +0000

    Add missing text

--- src/main/java/me/alexprogrammerde/PistonMOTD/api/PlaceholderParser.java
+++ src/main/java/me/alexprogrammerde/PistonMOTD/api/PlaceholderParser.java
@@ -2,7 +2,7 @@ package me.alexprogrammerde.PistonMOTD.api;
 
 public interface PlaceholderParser {
     /**
-     * Parse the placeholders of
+     * Parse the placeholders of a string
      * @param text Current string
      * @return Parsed string
      */
