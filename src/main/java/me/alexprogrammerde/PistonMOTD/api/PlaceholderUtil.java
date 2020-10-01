ac1da25b16e467b8699ae0c3779a04d1ae9bf80d
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2020-10-01 21:03:16 +0200

    Add API Guardian

--- src/main/java/me/alexprogrammerde/PistonMOTD/api/PlaceholderUtil.java
+++ src/main/java/me/alexprogrammerde/PistonMOTD/api/PlaceholderUtil.java
@@ -1,5 +1,8 @@
 package me.alexprogrammerde.PistonMOTD.api;
 
+import org.apiguardian.api.API;
+import org.bukkit.event.EventHandler;
+
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +18,7 @@ public class PlaceholderUtil {
      * @param text A string with placeholders to get parsed
      * @return A completely parsed string
      */
+    @API(status = API.Status.INTERNAL)
     public static String parseText(String text) {
         String returnedString = text;
 
@@ -29,14 +33,17 @@ public class PlaceholderUtil {
      * Register a parser to make him parse some placeholders
      * @param parser A parser to register
      */
+    @API(status = API.Status.STABLE)
     public static void registerParser(@Nonnull PlaceholderParser parser) {
         placeholders.add(parser);
     }
+
 
     /**
      * Unregister a parser to stop him from parsing
      * @param parser The parser to unregister
      */
+    @API(status = API.Status.STABLE)
     public static void unregisterParser(@Nonnull PlaceholderParser parser) {
         placeholders.remove(parser);
     }
