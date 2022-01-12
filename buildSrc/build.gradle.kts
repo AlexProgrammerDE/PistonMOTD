4e593b2f90175ab8cf60c8407fd8a56efe6ee36c
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-01-12 18:51:15 +0100

    Update java

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,3 +11,5 @@ dependencies {
     implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
     implementation("net.kyori:indra-common:2.0.6")
 }
+
+java.targetCompatibility = JavaVersion.VERSION_1_8
