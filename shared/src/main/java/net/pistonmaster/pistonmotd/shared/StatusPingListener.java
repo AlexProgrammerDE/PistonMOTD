85b5a927b5fff7cc0e0d104b6be5b276abf35978
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-01-25 14:44:47 +0100

    Fix error catching

--- shared/src/main/java/net/pistonmaster/pistonmotd/shared/StatusPingListener.java
+++ shared/src/main/java/net/pistonmaster/pistonmotd/shared/StatusPingListener.java
@@ -17,31 +17,51 @@ public interface StatusPingListener {
             ping.setDescription(MOTDUtil.getMOTD(config.getStringList("motd.text"), ping.supportsHex(), PlaceholderUtil::parseText));
         }
 
-        if (config.getBoolean("extended.hideplayers")) {
-            ping.setHidePlayers(true);
+        if (config.getBoolean("hideplayers")) {
+            try {
+                ping.setHidePlayers(true);
+            } catch (UnsupportedOperationException e) {
+                logUnsupportedConfig("protocol");
+            }
         } else {
-            if (config.getBoolean("extended.protocol.activated")) {
-                ping.setVersionName(PlaceholderUtil.parseText(config.getString("extended.protocol.text").replace("%aftericon%", afterIcon)));
+            if (config.getBoolean("protocol.activated")) {
+                try {
+                    ping.setVersionName(PlaceholderUtil.parseText(config.getString("extended.protocol.text").replace("%aftericon%", afterIcon)));
+                } catch (UnsupportedOperationException e) {
+                    logUnsupportedConfig("protocol");
+                }
             }
 
-            if (config.getBoolean("extended.overrideprotocolnumber.activated")) {
-                ping.setVersionProtocol(config.getInt("extended.overrideprotocolnumber.value"));
+            if (config.getBoolean("overrideprotocolnumber.activated")) {
+                try {
+                    ping.setVersionProtocol(config.getInt("extended.overrideprotocolnumber.value"));
+                } catch (UnsupportedOperationException e) {
+                    logUnsupportedConfig("overrideprotocolnumber");
+                }
             }
         }
 
         if (config.getBoolean("playercounter.bukkitplayercounter")) {
-            ping.clearSamples();
+            try {
+                ping.clearSamples();
 
-            for (PlayerWrapper player : plugin.getPlayers()) {
-                if (config.getStringList("hiddenplayers").contains(player.getName())) continue;
+                for (PlayerWrapper player : plugin.getPlayers()) {
+                    if (config.getStringList("hiddenplayers").contains(player.getName())) continue;
 
-                ping.addSample(player.getUniqueId(), player.getDisplayName());
+                    ping.addSample(player.getUniqueId(), player.getDisplayName());
+                }
+            } catch (UnsupportedOperationException e) {
+                logUnsupportedConfig("playercounter.bukkitplayercounter");
             }
-        } else if (config.getBoolean("extended.playercounter.activated")) {
-            ping.clearSamples();
+        } else if (config.getBoolean("playercounter.activated")) {
+            try {
+                ping.clearSamples();
 
-            for (String str : config.getStringList("extended.playercounter.text")) {
-                ping.addSample(UUID.randomUUID(), PlaceholderUtil.parseText(str));
+                for (String str : config.getStringList("extended.playercounter.text")) {
+                    ping.addSample(UUID.randomUUID(), PlaceholderUtil.parseText(str));
+                }
+            } catch (UnsupportedOperationException e) {
+                logUnsupportedConfig("playercounter");
             }
         }
 
@@ -49,8 +69,12 @@ public interface StatusPingListener {
             ping.setMax(config.getInt("overridemax.value"));
         }
 
-        if (config.getBoolean("extended.overrideonline.activated")) {
-            ping.setOnline(config.getInt("extended.overrideonline.value"));
+        if (config.getBoolean("overrideonline.activated")) {
+            try {
+                ping.setOnline(config.getInt("extended.overrideonline.value"));
+            } catch (UnsupportedOperationException e) {
+                logUnsupportedConfig("overrideonline");
+            }
         }
 
         if (config.getBoolean("icons")) {
@@ -60,6 +84,10 @@ public interface StatusPingListener {
                 ping.setFavicon(plugin.favicons.get(plugin.random.nextInt(0, plugin.favicons.size())));
             }
         }
+    }
+
+    default void logUnsupportedConfig(String value) {
+        getPlugin().warn("\"" + value + "\" was activated in the config, but your platform does not support this feature!");
     }
 
     PistonMOTDPlugin getPlugin();
