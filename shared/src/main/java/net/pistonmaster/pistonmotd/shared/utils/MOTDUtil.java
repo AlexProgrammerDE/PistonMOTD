8fdc59ec920401a733f453dd0abbe35cd26f5ac8
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-05-22 09:13:36 +0200

    Cleanup motd code

--- shared/src/main/java/net/pistonmaster/pistonmotd/shared/utils/MOTDUtil.java
+++ shared/src/main/java/net/pistonmaster/pistonmotd/shared/utils/MOTDUtil.java
@@ -10,20 +10,9 @@ public class MOTDUtil {
 
     public static String getMOTDJson(List<String> motds, boolean supportsHex) {
         String unparsedMOTD = motds.get(random.nextInt(motds.size()));
-
-        String[] split = unparsedMOTD.contains("%nohexmotd%") ? unparsedMOTD.split("%nohexmotd%") : new String[]{unparsedMOTD};
-
-        String returnedString;
-        if (split.length > 1) {
-            if (supportsHex) {
-                returnedString = split[0];
-            } else {
-                returnedString = split[1];
-            }
-        } else {
-            returnedString = split[0];
-        }
-
-        return PlaceholderUtil.parseTextToJson(returnedString, supportsHex);
+        String[] split = unparsedMOTD.split("%nohexmotd%", 2);
+        return PlaceholderUtil.parseTextToJson(
+                split.length == 2 ? supportsHex ? split[0] : split[1] : split[0],
+                supportsHex);
     }
 }
