b855bb66f9379d40bb0070a77e3a96785e721181
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-12-16 16:26:21 +0100

    Update Velocity version

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -11,12 +11,12 @@ dependencies {
 
     implementation("org.bstats:bstats-velocity:3.1.0")
 
-    compileOnly("com.velocitypowered:velocity-api:3.1.1")
-    annotationProcessor("com.velocitypowered:velocity-api:3.1.1")
+    compileOnly("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
+    annotationProcessor("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
 }
 
 tasks {
     runVelocity {
-        version("3.1.1")
+        version("3.4.0-SNAPSHOT")
     }
 }
