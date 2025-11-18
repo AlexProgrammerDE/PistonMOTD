f61810cf7c07f339681998084b295ab55cafa2fb
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-11-18 20:46:35 +0100

    chore: add runpaper

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("pm.platform-conventions")
     id("pm.kyori-conventions")
+    id("xyz.jpenilla.run-paper") version "3.0.2"
 }
 
 dependencies {
@@ -15,4 +16,23 @@ dependencies {
 
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("jakarta.annotation:jakarta.annotation-api:3.0.0")
+}
+
+tasks {
+  runServer {
+    minecraftVersion("1.21.10")
+
+    jvmArgs = listOf("-Dcom.mojang.eula.agree=true")
+  }
+}
+
+runPaper {
+  folia {
+    registerTask {
+      minecraftVersion("1.21.10")
+
+      jvmArgs = listOf("-Dcom.mojang.eula.agree=true")
+      args = listOf("--nogui")
+    }
+  }
 }
